2173c94f4b309b1c5ab63aeadcd201eea1f9a02c
colecharb <40375497+colecharb@users.noreply.github.com>
2025-05-08 00:20:01 -0400

    fix(token): adds <pre> tag

--- src/components/ui/token.tsx
+++ src/components/ui/token.tsx
@@ -1,5 +1,9 @@
 function Token({ token }: { token: string }) {
-  return <div className='border bg-muted rounded-md px-2 py-1'>{token}</div>;
+  return (
+    <div className='border bg-muted rounded-md px-2 py-1'>
+      <pre>{token}</pre>
+    </div>
+  );
 }
 
 export { Token };
