a41dad115f379ecea32c74b2971fd46e883ec8cb
colecharb <40375497+colecharb@users.noreply.github.com>
2025-05-07 23:09:31 -0400

    feat: defines dark hook

---
+++ src/hooks/useIsDark.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+function useIsDark() {
+  const [isDark, setIsDark] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return document.documentElement.classList.contains('dark');
+    }
+    return true;
+  });
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDark]);
+
+  return [isDark, setIsDark] as const;
+}
+
+export { useIsDark };
