85ee8c6df1388c2f24a134fbc9e1d9adaccba84a
colecharb <40375497+colecharb@users.noreply.github.com>
2025-05-08 16:30:39 -0400

    feat(token): overhauls component; accepts hovered and selected bools, as well as additional className props

--- src/components/ui/token.tsx
+++ src/components/ui/token.tsx
@@ -1,6 +1,29 @@
-function Token({ token }: { token: string }) {
+import { cn } from '@/lib/utils';
+import type { ClassNameValue } from 'tailwind-merge';
+
+function Token({
+  token,
+  hovered,
+  selected,
+  className,
+}: {
+  token: string;
+  hovered?: boolean;
+  selected?: boolean;
+  className?: ClassNameValue;
+}) {
+  const hoverClass = 'bg-gray-500';
+  const selectedClass = 'bg-pink-500';
+
   return (
-    <div className='text-xs bg-muted rounded-md px-2 py-1'>
+    <div
+      className={cn(
+        'text-sm bg-muted rounded-md px-2 py-1',
+        className,
+        hovered && hoverClass,
+        selected && selectedClass,
+      )}
+    >
       <pre>{token}</pre>
     </div>
   );
