605a294565afa1c048c501d091c8c96da0701ee5
colecharb <40375497+colecharb@users.noreply.github.com>
2025-05-08 09:16:55 -0400

    fix(token): removes border

--- src/components/ui/token.tsx
+++ src/components/ui/token.tsx
@@ -1,6 +1,6 @@
 function Token({ token }: { token: string }) {
   return (
-    <div className='border bg-muted rounded-md px-2 py-1'>
+    <div className='text-xs bg-muted rounded-md px-2 py-1'>
       <pre>{token}</pre>
     </div>
   );
